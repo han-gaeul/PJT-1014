fa1dbeeb05e966a907610f4867f356a86be3f163
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-14 14:29:39 +0900

    accounts/index

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,14 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+# 회원가입
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('accountsLindex')
+            return redirect('accounts:signup')
     else:
         form = CustomUserCreationForm()
     context = {
@@ -18,6 +19,7 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
 
+# 로그인
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -30,3 +32,7 @@ def login(request):
         'form' : form
     }
     return render(request, 'accounts/login.html', context)
+
+# 회원 목록
+def index(request):
+    return render(request, 'accounts/index.html')
