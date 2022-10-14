ba028db2feb4e89391d10f6c82df4b844f88f44a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-14 16:37:47 +0900

    reviews/create

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'reviews',
     'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
